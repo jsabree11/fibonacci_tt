c565d81a2010627adf60d623a365a3490bd9f18a
jsabree11 <jakesabree11@gmail.com>
2026-05-12 19:06:45 +0200

    Add Fibonacci checker design and testbench

--- src/project.v
+++ src/project.v
@@ -1,27 +1,42 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_jsabree11_fibonacci_checker (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg is_fib;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    always @(*) begin
+        case (ui_in)
+            8'd0,
+            8'd1,
+            8'd2,
+            8'd3,
+            8'd5,
+            8'd8,
+            8'd13,
+            8'd21,
+            8'd34,
+            8'd55,
+            8'd89,
+            8'd144,
+            8'd233: is_fib = 1'b1;
+            default: is_fib = 1'b0;
+        endcase
+    end
+
+    assign uo_out = {7'b0, is_fib};
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+    wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
